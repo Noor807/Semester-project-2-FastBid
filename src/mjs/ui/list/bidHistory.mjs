372d61c456c9f2d3dbc5c7884eb30c4e0dbc92b8
Noor Irfan <152153984+Noor807@users.noreply.github.com>
2025-02-26 22:49:58 +0100

    add styling on bid history

--- src/mjs/ui/list/bidHistory.mjs
+++ src/mjs/ui/list/bidHistory.mjs
@@ -2,15 +2,17 @@ export async function createBidHistoryModal(bids) {
   const modal = document.createElement("div");
   modal.id = "bidHistoryModal";
   modal.className =
-    "fixed inset-0 bg-black  mt-20 bg-opacity-50 mt-15 flex justify-center items-center hidden";
+    "fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center hidden";
 
   const modalContent = document.createElement("div");
-  modalContent.className = "bg-white p-4  rounded-lg w-96 shadow-lg";
+  modalContent.className =
+    "bg-white p-4 rounded-lg w-96 max-h-[80vh] overflow-y-auto shadow-lg"; // Adjusted for scroll and responsive height
 
   const closeButton = document.createElement("button");
   closeButton.innerText = "Close";
   closeButton.className = "text-red-500 font-bold float-right";
   closeButton.onclick = () => (modal.style.display = "none");
+
   const sortList = bids.sort((a, b) => b.amount - a.amount);
 
   modalContent.appendChild(closeButton);
@@ -18,7 +20,7 @@ export async function createBidHistoryModal(bids) {
   sortList.forEach((bid) => {
     const bidContainer = document.createElement("div");
     bidContainer.className =
-      "flex justify-between  items-center w-full border-b py-2 md:py-4";
+      "flex justify-between items-center w-full border-b py-2 md:py-4";
 
     const userInfo = document.createElement("div");
     userInfo.className = "flex items-center space-x-4";
@@ -41,9 +43,6 @@ export async function createBidHistoryModal(bids) {
 
     bidContainer.appendChild(userInfo);
     bidContainer.appendChild(bidAmount);
-
-    const modalContent = document.querySelector(".modal-content");
-    modalContent.className = "max-h-[400px] overflow-y-auto";
 
     modalContent.appendChild(bidContainer);
   });
