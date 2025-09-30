a16c812182d3657f78bbb2d3cb6cda30eb80a7d0
Noor Irfan <152153984+Noor807@users.noreply.github.com>
2025-09-30 13:02:48 +0200

    formatting login.mjs

--- src/mjs/api/auth/login.mjs
+++ src/mjs/api/auth/login.mjs
@@ -11,7 +11,6 @@
 import { fetchAuctionProfile } from "../profile/auctionProfile.mjs";
 import { API_AUTH_LOGIN } from "../constants";
 
-
 export async function login({ email, password }) {
   try {
     const response = await fetch(API_AUTH_LOGIN, {
